882642324afc6f6df2e2495ae8393bfe008e511d
fathlaziss <71271714+fathlaziss@users.noreply.github.com>
2020-12-28 16:59:41 +0700

    [Muhamad Fathul Azis]
    API finish and ready to use

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,24 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Laravel\Jetstream\HasTeams;
+use Laravel\Sanctum\HasApiTokens;
+use Laravel\Jetstream\HasProfilePhoto;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use HasTeams;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -20,6 +30,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'picturePath',
+        'address',
+        'houseNumber',
+        'phoneNumber',
+        'city',
+        'roles',
     ];
 
     /**
@@ -30,6 +46,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
     ];
 
     /**
@@ -40,4 +58,23 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
+
+    public function getCreatedAtAttribute($created_at)
+    {
+        return Carbon::parse($created_at)->timestamp;
+    }
+
+    public function getUpdatedAtAttribute($updated_at)
+    {
+        return Carbon::parse($updated_at)->timestamp;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\API\MidtransController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FoodController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +17,23 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//Homepage
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
+
+// Dashboard
+Route::prefix('dashboard')->middleware('auth:sanctum', 'admin')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('users', UserController::class);
+    Route::resource('food', FoodController::class);
+
+    Route::get('transactions/{id}/status/{status}', [TransactionController::class, 'changeStatus'])
+        ->name('transactions.changeStatus');
+    Route::resource('transactions', TransactionController::class);
+});
+
+// Midtrans Related
+Route::get('midtrans/success', [MidtransController::class, 'success']);
+Route::get('midtrans/unfinish', [MidtransController::class, 'unfinish']);
+Route::get('midtrans/error', [MidtransController::class, 'error']);
